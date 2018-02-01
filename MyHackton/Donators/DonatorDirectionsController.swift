9e6d12a41454535a5c174a7461a83372c55421bf
מתןפור <matan@matans-macbook.local>
2018-02-01 11:31:10 +0200

    added folders and addresses table

---
+++ MyHackton/Donators/DonatorDirectionsController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class DonatorDirectionsController: UIViewController,UITableViewDataSource {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        return cell
+    }
+
+    
+
+}
