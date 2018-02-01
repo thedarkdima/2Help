9da8281cbf2495966d231788b055947e7fb45451
מתןפור <matan@matans-macbook.local>
2018-02-02 00:56:25 +0200

    delivery adresses for delivery guy with table and icons

---
+++ MyHackton/Delivery/DeliveriesAddressesCell.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class DeliveriesAddressesCell: UITableViewCell {
+
+    @IBOutlet var status_icon: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
