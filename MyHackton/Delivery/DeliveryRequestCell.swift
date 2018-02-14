86221e2c16a7189d378ee34de692e25e66896e02
מתןפור <matan@matans-macbook.local>
2018-02-14 19:03:32 +0200

    .

--- MyHackton/Delivery/DeliveryRequestCell.swift
+++ MyHackton/Delivery/DeliveryRequestCell.swift
@@ -3,6 +3,7 @@ import UIKit
 class DeliveryRequestCell: UITableViewCell {
     
     @IBOutlet var adress: UILabel!
+    @IBOutlet var icon_adress: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
