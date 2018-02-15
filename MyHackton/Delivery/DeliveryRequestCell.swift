96cfbfa7ed278ace5f17a16d97d07fe2af159d3d
Alona Tov <alonatov@alonas-macbook-air.local>
2018-02-15 18:54:29 +0200

    Login and stuff

--- MyHackton/Delivery/DeliveryRequestCell.swift
+++ MyHackton/Delivery/DeliveryRequestCell.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class DeliveryRequestCell: UITableViewCell {
     
-    @IBOutlet var adress: UILabel!
+    @IBOutlet var address: UILabel!
     @IBOutlet var icon_adress: UIImageView!
     
     override func awakeFromNib() {
