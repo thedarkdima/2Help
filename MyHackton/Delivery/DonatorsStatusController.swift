bafd85df50e35b48870515aeb88171828d37a1ff
מתןפור <matan@matans-macbook.local>
2018-02-25 16:30:26 +0200

    waze working!

--- MyHackton/Delivery/DonatorsStatusController.swift
+++ MyHackton/Delivery/DonatorsStatusController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreLocation
 
 class DonatorsStatusController: UIViewController {
     
@@ -52,11 +53,32 @@ class DonatorsStatusController: UIViewController {
         //check if waze installed on the iPhone
         if(UIApplication.shared.canOpenURL(URL(string:"waze://")!)){
             //to be able to read, the spaces need to be replaced with "+"
-            let address = (donatorsAddressLbl.text?.replacingOccurrences(of: " ", with: "+"))!
-            let addressURL = URL(string: "https://waze.com/ul?q=\(address)")
+          ///  let address = (donatorsAddressLbl.text?.replacingOccurrences(of: " ", with: "+"))!
+//            let address = "tel+aviv+yafo"
+//            let addressURL = URL(string: "https://waze.com/ul?q=\(address)")
             
-            UIApplication.shared.open(addressURL!, options: [:], completionHandler: nil)
-            UIApplication.shared.isIdleTimerDisabled = true
+            // geocoding
+            
+            let addressTry = "פתח תקווה רוטשילד 119"
+            let geoCoder = CLGeocoder()
+            
+            geoCoder.geocodeAddressString(addressTry, completionHandler: { (placemarks, error) in
+                let longitude = placemarks?.first?.location?.coordinate.longitude
+                let latitude = placemarks?.first?.location?.coordinate.latitude
+                
+                
+                let addressURL = URL(string: "http://waze.com/ul?ll=\(latitude!),\(longitude!)&navigate=yes")
+               // let addressURL = URL(string: "http://waze.com/ul?q=45.6906304,-120.810983&navigate=yes")
+                
+                UIApplication.shared.open(addressURL!, options: [:], completionHandler: nil)
+                UIApplication.shared.isIdleTimerDisabled = true
+                
+            })
+            
+            ////
+            
+//            UIApplication.shared.open(addressURL!, options: [:], completionHandler: nil)
+//            UIApplication.shared.isIdleTimerDisabled = true
         }else {
             //else open a web url that says waze is not installed and link to appstore to download waze
             let url = URL(string:"http://www.itunes.apple.com/us/app/id323229106")!
