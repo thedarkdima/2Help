1635d994e8b9f9f531e7f4b7ac3a9924c6c4d4c8
מתןפור <matan@matans-macbook.local>
2018-03-01 15:32:46 +0200

    1

--- MyHackton/Donators/Map/MapAddressController.swift
+++ MyHackton/Donators/Map/MapAddressController.swift
@@ -1,10 +1,11 @@
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapAddressController: UIViewController , MKMapViewDelegate {
+class MapAddressController: UIViewController , CLLocationManagerDelegate {
 
     @IBOutlet weak var MyMap: MKMapView!
-    var locationManager: CLLocationManager!
+    var locationManager = CLLocationManager()
     
     override func viewWillAppear(_ animated: Bool) {
         tabBarController?.navigationItem.title = tabBarItem.title
@@ -13,36 +14,43 @@ class MapAddressController: UIViewController , MKMapViewDelegate {
         
         tabBarController?.navigationItem.leftBarButtonItem = b
         self.navigationItem.hidesBackButton = true
-        
+    
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
-        locationManager =  CLLocationManager()  //initialize location manager
-        locationManager.requestAlwaysAuthorization()  //prompt user current location
-        MyMap.delegate = self
-        MyMap.showsUserLocation = true // showing user current location
-        MyMap.mapType = .standard // regular map 
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
+        MyMap.mapType = .standard // regular map
         
     }
     
-    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        let newPlace = userLocation.coordinate
-        MyMap.setRegion(MKCoordinateRegionMakeWithDistance(newPlace, 8000, 8000), animated: true)
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        MyMap.setRegion(region, animated: true)
+        MyMap.showsUserLocation = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+//    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+//        let newPlace = userLocation.coordinate
+//        MyMap.setRegion(MKCoordinateRegionMakeWithDistance(newPlace, 8000, 8000), animated: true)
+//    }
     
     func addMyMarkers(name:String, PinLatitude:Double, PinLongitude:Double ){
         let pin = MKPointAnnotation()
         pin.title = "\(name)"
         pin.coordinate = CLLocationCoordinate2D(latitude: PinLatitude, longitude: PinLongitude)
-        MyMap.addAnnotation(pin) // add to map
+        
+      //  MyMap.addAnnotation(pin) // add to map
+        
     }
     
     //logout from the system
