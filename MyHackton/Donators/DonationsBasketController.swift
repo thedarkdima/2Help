0982eb541b9c0ea0f2ebc5938e7725e4aeab3833
Alona Tov <alonatov@alonas-macbook-air.local>
2018-03-11 17:43:44 +0200

    working on manager

--- MyHackton/Donators/DonationsBasketController.swift
+++ MyHackton/Donators/DonationsBasketController.swift
@@ -4,12 +4,33 @@ class DonationsBasketController: UIViewController, UITableViewDataSource, UITabl
     
     var scannedCode = ""
     var backButton : UIBarButtonItem!
+    //Manager
+    var manager = false
+    var token = ""
+    var request: [String] = []
+    var items: [[String]] = [[]]
+    @IBOutlet var table: UITableView!
+    //
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationItem.title = "סל תרומות"
-        print(scannedCode)
+        if manager{
+            let prefs = UserDefaults.standard
+            if let tok = prefs.string(forKey: "token"){
+                token = tok
+                navigationItem.title = "הוסף למלאי"
+                ServerConnections.getDoubleArrayAsync("/request_items", [token, request[0]], handler: {itemsArray in
+                    if let array = itemsArray{
+                        self.items = array
+                    }
+                })
+            }
+        } else {
+            navigationItem.title = "סל תרומות"
+            print(scannedCode)
+            
+        }
     }
     
     //move to scannerViewController to scan barcodes
@@ -21,11 +42,25 @@ class DonationsBasketController: UIViewController, UITableViewDataSource, UITabl
     
     //table view functions//
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 5
+        if(manager){
+            if items[0].count > 0{
+                return items.count
+            } else {
+                return 0
+            }
+        } else {
+            return 5
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "products_cell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "products_cell")! as! ProductsTableViewCell
+        if manager{
+            cell.name.text = items[indexPath.row][1]
+            cell.counter.text = items[indexPath.row][2]
+        } else {
+            
+        }
         return cell
     }
     ////
@@ -37,6 +72,25 @@ class DonationsBasketController: UIViewController, UITableViewDataSource, UITabl
         present(alert, animated: true, completion: nil)
     }
     
-  
-
+    @IBAction func addItemsManager(_ sender: Any) {
+        var flag = false
+        for i in 0...items.count - 1{
+            let item = table.visibleCells[i] as! ProductsTableViewCell
+            if(items[i][1] != item.name.text!){
+                flag = true
+                if(items[i][2] != item.counter.text!){
+                    flag = true
+                }
+            }
+        }
+        if flag{
+            showAlert()
+        } else {
+            addItems()
+        }
+    }
+    
+    func addItems(){
+        
+    }
 }
--- MyHackton/StorageKeeper/MessengersListController.swift
+++ MyHackton/StorageKeeper/MessengersListController.swift
@@ -2,28 +2,50 @@ import UIKit
 
 class MessengersListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var donatorsAddresses : [String] = []
-    var messangersNames : [String] = []
+    var requests: [[String]] = [[]]
+    @IBOutlet weak var requestsList: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let prefs = UserDefaults.standard
+        if let token = prefs.string(forKey: "token"){
+            ServerConnections.getDoubleArrayAsync("/requests", [token, "נלקח"], handler: { requestsArray in
+                if let array = requestsArray{
+                    self.requests = array
+                    self.requestsList.reloadData()
+                } else {
+                    //Move back to the main page
+                    self.navigationController?.popToRootViewController(animated: true)
+                }
+            })
+        } else {
+            //Move back to the main page
+            navigationController?.popToRootViewController(animated: true)
+        }
     }
     
     @IBAction func toSuppliesPage(_ sender: UIButton) {
-        let suppliesPage = storyboard!.instantiateViewController(withIdentifier: "supplies")
+        let suppliesPage = storyboard!.instantiateViewController(withIdentifier: "supplies") as! DonationsBasketController
+        suppliesPage.manager = true
         present(suppliesPage, animated: true, completion: nil)
-        
     }
     
     
     //table functions//
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if(requests[0].count > 0){
+            return requests.count
+        } else {
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "messanger")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messanger")! as! MessengerListCell
+        if(requests[0].count > 0){
+            cell.address.text = requests[indexPath.row][2]
+            cell.deliveryName.text = requests[indexPath.row][6]
+        }
         return cell
     }
     ////
