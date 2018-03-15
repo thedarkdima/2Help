7af23a95ca5cabbad1587422e00a57680c2b0fbd
מתןפור <matan@matans-macbook.local>
2018-03-15 21:41:36 +0200

    fix

--- MyHackton/Design/collecViewCell.swift
+++ MyHackton/Design/collecViewCell.swift
@@ -7,11 +7,13 @@ class collecViewCell: UICollectionViewCell {
     @IBOutlet var webView: WKWebView!
   
     
-    let urls : [URL] = [URL(string: "https://d.ibtimes.co.uk/en/full/1464094/baby-hand.jpg?w=700")!,
+    let urls : [URL] = [
+                        URL(string: "https://d.ibtimes.co.uk/en/full/1464094/baby-hand.jpg?w=700")!,
+                        URL(string: "https://www.youtube.com/embed/ucts039wCPo" )!,
                         URL(string: "https://www.yumama.com/thumbnail.php?file=2014/07/najpopularnija_zenska_imena_u_hrvatskoj_aps_978079305.jpg&size=article_large" )!,
                         URL(string: "http://alwaysbusymama.com/images/content/383-zdorovyj-detskij-son-10-pravil.jpg")!,
-                        URL(string: "https://www.youtube.com/embed/ucts039wCPo" )!,
                         URL(string: "https://m.static.lagardere.cz/frekvence1/edee/clanky/21974/kluk.jpg")!
+                        
     ]
     
     override func awakeFromNib() {
